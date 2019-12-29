f093f12bc54ac08d6053fa0ccae674bb50361343
leejeongmin <danmin20@khu.ac.kr>
2019-12-29 13:53:48 +0900

    Updates

---
+++ precache-manifest.a80fb56a20ee4890fba6e709435a715f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa65fe0d5e3f0429858eadd88d9e6c8c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0060011415f71ebb2660",
+    "url": "/movie_app/static/css/main.696b7c65.chunk.css"
+  },
+  {
+    "revision": "29c5e941d7fea8d8af55",
+    "url": "/movie_app/static/js/2.f06b77d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.f06b77d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0060011415f71ebb2660",
+    "url": "/movie_app/static/js/main.8c1c2ccc.chunk.js"
+  },
+  {
+    "revision": "856a8219dbb64a483704",
+    "url": "/movie_app/static/js/runtime-main.060affd4.js"
+  }
+]);
